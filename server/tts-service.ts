7bc6e804ba77f10ec730cd46049a52f711c35223
mikefish <mfischthal@kingschoolct.org>
2025-10-15 12:52:55 -0400

    model download

--- server/tts-service.ts
+++ server/tts-service.ts
@@ -92,13 +92,13 @@ class IndexTtsService extends EventEmitter {
   }
 
   private broadcast(message: TtsWsMessage) {
-    for (const subscriber of this.subscribers) {
+    this.subscribers.forEach((subscriber) => {
       try {
         subscriber(message);
       } catch (err) {
         console.error("TTS subscriber error:", err);
       }
-    }
+    });
   }
 
   public subscribe(listener: (message: TtsWsMessage) => void): () => void {
@@ -173,11 +173,11 @@ class IndexTtsService extends EventEmitter {
     const pythonArgs = [
       "-u",
       this.workerScript,
-      command,
       "--root-dir",
       this.rootDir,
       "--models-dir",
       this.modelsDir,
+      command,
       ...args,
     ];
 
